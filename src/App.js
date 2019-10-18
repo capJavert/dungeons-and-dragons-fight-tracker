975a4aad68d404c5a87e82b25eb086e05076edf1
Ante Barić <ante.baric@burza.hr>
2019-10-18 13:11:17 +0200

    inject map through url param

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [map, setMap] = useState()
+    useEffect(() => {
+        const { href } = window.location
+        const url = new URL(href)
+        const map = url.searchParams.get('map')
+
+        setMap(map)
+    }, [])
+
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', background: '#000000' }}>
+            {false && <div style={{ display: 'flex', flex: 1, backgroundImage: `url("${map}")`, backgroundSize: 'contain', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }} />}
+            <div>
+                <img style={{ width: '100%', height: 'auto' }} src={map} alt="Map" />
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default App
